1a96763e69d58697535dfd80d4111f96cd147333
Michael Law <rmdashrfv@hey.com>
2022-07-20 11:46:44 -0400

    Added flexbox and transition

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
+      {
+	fighters.map((element, index) => {
+	  return(
+	    <div>
+	      <img src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${element.toLowerCase()}.png`} />
+	    </div>
+	  ) 
+	})
+      }
       </div>
     </div>
   );
