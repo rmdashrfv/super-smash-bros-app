b38ec0dc0c53d0891e23bbcc21d4a250f2b66d88
Michael Law <rmdashrfv@hey.com>
2022-07-21 16:32:54 -0400

    Added CSS

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,8 +1,9 @@
 const FighterScreen = ({ selectedFighter }) => {
   return(
     <div id="fighter-screen" style={{backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/bg.jpg)`}}>
-      <img src="" className="char-model" />
+      <img src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/main.png`} className="char-model" />
       <h3 className="char-name">{selectedFighter}</h3>
+      <h3 className="char-name-bg">{selectedFighter}</h3>
     </div>
   )
 }
