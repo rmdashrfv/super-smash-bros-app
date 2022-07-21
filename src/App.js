6ef831af755f5226d9766e6203abe904b673a872
Michael Law <rmdashrfv@hey.com>
2022-07-21 15:52:53 -0400

    added fighter screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import { useState } from 'react'
 import Fighter from './components/Fighter'
+import FighterScreen from './components/FighterScreen'
 
 function App() {
   const fighters = [
@@ -16,6 +18,9 @@ function App() {
     {name: 'Zelda', color: 'midnightblue'},
     {name: 'Ken', color: 'firebrick'}
   ]
+  
+  const [selectedFighter, setSelectedFighter] = useState()
+
   return (
     <div className="App">
       <h1>Fighters</h1>
@@ -23,11 +28,17 @@ function App() {
       {
 	fighters.map((element, index) => {
 	  return(
-	    <Fighter fighter={element} />
+	    <Fighter fighter={element} setSelectedFighter={setSelectedFighter} />
 	  ) 
 	})
       }
       </div>
+      {
+	// Conditional render (based on ternary)
+	selectedFighter ?
+        <FighterScreen />
+	: null
+      }
     </div>
   );
 }
